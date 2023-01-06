682aba70c6cdac5c9fa69a62bccce900fc7ee61a
티맥스인공지능센터 <120453710+TmaxAIcenter@users.noreply.github.com>
2023-01-06 10:18:07 +0900

    Add files via upload

---
+++ dataset_download.sh
@@ -0,0 +1,13 @@
+mkdir Dataset
+cd Dataset
+mkdir FU
+cd FU
+wget https://github.com/TmaxAIcenter/Bivalve-UNet-master/releases/download/dataset/FU_test.zip
+unzip FU_test.zip
+rm FU_test.zip
+cd ..
+mkdir MA
+cd MA
+wget https://github.com/TmaxAIcenter/Bivalve-UNet-master/releases/download/dataset/MA_test.zip
+unzip MA_test.zip
+rm MA_test.zip
